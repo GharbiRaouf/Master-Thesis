7168ecc235550b70408e8442ec9b3af610c54855
Raouf Gharbi <gharbiraouf@outlook.com>
2018-12-17 08:53:04 +0100

    reassign api_url

--- canvas-maker/Backend/application/backend_dashboard/src/utils/server_constants.js
+++ canvas-maker/Backend/application/backend_dashboard/src/utils/server_constants.js
@@ -1,2 +1,2 @@
 export const SERV_URL="http://h2793844.stratoserver.net:5000/"
-export const API_URL="http://127.0.0.1:5000/api/v1/"
+export const API_URL="http://h2793844.stratoserver.net:5000/api/v1/"
