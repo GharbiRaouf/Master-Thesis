c312a51da028fa5cfb57e934d4e5435594adc6ed
Raouf Gharbi <gharbiraouf@outlook.com>
2018-12-17 09:21:52 +0100

    working on ports

--- canvas-maker/Frontend/src/constants/utils.js
+++ canvas-maker/Frontend/src/constants/utils.js
@@ -1,2 +1,3 @@
-export const SERVER_URL="h2793844.stratoserver.net/"
-export const API_URL="http://127.0.0.1:5000/api/v1/"
+export const SERVER_URL="http://h2793844.stratoserver.net:5000/"
+// export const API_URL="http://127.0.0.1:5000/api/v1/"
+export const API_URL="http://h2793844.stratoserver.net:5000/api/v1/"
