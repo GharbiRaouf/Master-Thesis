620b1affff9a539563d41b5ff8140e1b4c320b2d
Yassin KISRAWI <yassinkisrawi42@gmail.com>
2018-12-22 11:40:09 +0100

    linking containers

--- canvas-maker/Frontend/src/constants/utils.js
+++ canvas-maker/Frontend/src/constants/utils.js
@@ -1,7 +1,7 @@
 //deployement config
-// export const SERVER_URL="http://h2793844.stratoserver.net:5000/"
-// export const FRONTEND_URL="http://h2793844.stratoserver.net/"
-// export const API_URL="http://h2793844.stratoserver.net:5000/api/v1/"
+export const SERVER_URL="http://h2793844.stratoserver.net:5000/"
+export const FRONTEND_URL="http://h2793844.stratoserver.net/"
+export const API_URL="http://h2793844.stratoserver.net:5000/api/v1/"
 
 // local work
 // export const API_URL="http://127.0.0.1:5000/api/v1/"
@@ -10,6 +10,6 @@
 // export const FRONTEND_URL="http://127.0.0.1:3000/"
 
 //docker config
-export const API_URL= "http://"+process.env.SERVER_URL+":5000/api/v1/";
-export const SERVER_URL="http://"+process.env.SERVER_URL+":5000/"
-export const FRONTEND_URL="http://"+process.env.SERVER_URL+":3000/"
+// export const API_URL= "http://"+process.env.SERVER_URL+":5000/api/v1/";
+// export const SERVER_URL="http://"+process.env.SERVER_URL+":5000/"
+// export const FRONTEND_URL="http://"+process.env.SERVER_URL+":3000/"
